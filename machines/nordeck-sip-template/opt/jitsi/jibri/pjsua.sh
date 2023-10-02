02e5b5f6a9ef2acbe3c3048bf353ed818e8b3a70
emrah <emrah.eryilmaz@nordeck.net>
2023-10-02 18:36:50 +0300

    nordeck-sip-template, feat: add templates

---
+++ machines/nordeck-sip-template/opt/jitsi/jibri/pjsua.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+set -e
+
+# push display :0 view to virtual camera 1
+sudo systemctl start virtual-camera-1
+sleep 0.8
+
+REGISTRAR=$(echo "$@" | egrep -o "..registrar=sip:[^ ]" || true)
+AUTO_ANSWER=$(echo "$@" | egrep -o "..auto-answer-timer=[0-9]+" || true)
+
+# if auto-answer is set but there is no registrar, then this should be a direct
+# incoming call. Use the customized command in this case.
+if [[ -z "$REGISTRAR" && -n "$AUTO_ANSWER" ]]; then
+    TIMER=$(echo $AUTO_ANSWER | cut -d '=' -f2)
+
+    exec /usr/local/bin/pjsua --config-file /etc/jitsi/jibri/pjsua.config \
+        --auto-answer-timer=$TIMER --auto-answer=200 >/dev/null
+else
+    exec /usr/local/bin/pjsua --config-file /etc/jitsi/jibri/pjsua.config \
+        "$@" >/dev/null
+fi
