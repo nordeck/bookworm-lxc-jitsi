56ebee68f16c477be42df52f170ceb200a596327
emrah <emrah.eryilmaz@nordeck.net>
2023-11-13 13:29:43 +0300

    nordeck-jitsi, refactor: explicitly install lua5.3

--- installer-sub-scripts/nordeck-jitsi/307-jitsi.sh
+++ installer-sub-scripts/nordeck-jitsi/307-jitsi.sh
@@ -161,6 +161,7 @@ apt-get $APT_PROXY -y install gnupg
 apt-get $APT_PROXY -y install ngrep jq
 apt-get $APT_PROXY -y install ruby-hocon
 apt-get $APT_PROXY -y install openjdk-17-jre-headless
+apt-get $APT_PROXY -y install lua5.3
 EOS
 
 # ssl packages
